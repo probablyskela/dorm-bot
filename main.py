1eba4458eeb695317a7748dac00a906d668e51df
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2023-09-05 19:13:33 +0300

    Add reply with copypaste once in a while

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("Hello, World!")
+import logging
+import os
+import random
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+COPYPASTE = os.getenv('COPYPASTE')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if random.randint(1, 300) == 69:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=COPYPASTE,
+                                       reply_to_message_id=update.effective_message.id)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token=TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
+    application.run_polling()
